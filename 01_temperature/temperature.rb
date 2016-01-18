d9ff54a939b337a3984a0eb658412c3394639080
naemcivic <naemcivic@gmail.com>
2016-01-18 14:41:10 -0500

    Compelete exercise 2, all tests passed

---
+++ 01_temperature/temperature.rb
@@ -0,0 +1,28 @@
+def ftoc(temperature)
+  (temperature - 32) * 5 / 9
+  # if temperature == 32                   These were basic tests
+    # 0
+  # elsif temperature == 212
+    # 100
+  # elsif temperature == 68
+    # 20
+  # else
+    # 37
+  # end
+end
+
+  def ctof(temperature)
+    (temperature * 9) / 5 + 32
+
+
+    # if temperature == 0                   These were basic tests
+      # 32
+    # elsif temperature == 100
+      # 212
+    # else
+      # 68
+  # end
+
+
+
+end
